887bd21a10bf59753486f5ebe965a6ab08edbe5f
Sasha <sasadedenko28@gmail.com>
2019-09-17 19:02:55 +0300

    added first task

---
+++ lesson_1/1.1.1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a , b;
+      cout << "Say first number ";
+       cin >> a;
+      cout << "Say second number ";
+       cin >> b;
+      cout << "Your answer =) ";
+      cout << a+b;
+}
