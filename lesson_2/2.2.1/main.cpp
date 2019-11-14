3fe84e6a3b136ef89fc6c66c7d263b7868790765
Sasha <sasadedenko28@gmail.com>
2019-11-14 15:23:42 +0200

    fixd

---
+++ lesson_2/2.2.1/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int s;
+    int mas[10];
+
+    for(int i=0;i<10;i++)
+    {
+        cout << "Vvedi nomer kakoi-to shkolu: ";
+        cin  >> mas[i];
+        cout << endl;
+    }
+
+    cout << "A teper vvedi svoyu shkolu: ";
+    cin  >> s;
+    cout << endl;
+
+    for(int i=0;i<10;i++)
+    {
+        if(mas[i]== s)
+        {
+            cout << "Ya znayu etu shkolu! \n";
+            return 0;
+        }
+    }
+    for(int i=0;i<10;i++)
+    {
+        if (mas[i] != s)
+        {
+            cout << "Ya NE znayu etu shkolu! \n";
+            break;
+        }
+    }
+
+}
